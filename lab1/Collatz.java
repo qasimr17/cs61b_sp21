1207fba80e3834f7459f54b646fc2c78461c26d8
unknown <qasimr.1097@gmail.com>
2022-04-23 23:44:00 +0500

    Completed lab1

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -5,13 +5,16 @@ public class Collatz {
 
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
-            return n * 2;
+        int next = n;
+        if (n % 2 == 0){
+            next = n / 2;
         }
+
+        else if (n % 2 == 1){
+            next =  3*n + 1;
+        }
+
+        return next;
     }
 
     public static void main(String[] args) {
